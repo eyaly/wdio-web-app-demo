98455cb836904a04bb08edaf3508e3c5aed224a1
Eyal Yovel <eyalyovel@gmail.com>
2021-02-05 16:13:00 +0000

    add repo

---
+++ test/specs/login.spec.js
@@ -0,0 +1,27 @@
+import { LOGIN_USERS } from '../configs/e2eConstants';
+import LoginPage from '../page-objects/LoginPage';
+import SwagOverviewPage from '../page-objects/SwagOverviewPage'
+
+describe('LoginPage', () => {
+    beforeEach(() => {
+        browser.url('');
+        LoginPage.waitForIsShown();
+    });
+
+    it('should be able to test loading of login page', () => {
+        expect(LoginPage.waitForIsShown()).toEqual(
+            true,
+            'LoginPage page was not shown',
+        );
+    });
+
+    it('should be able to login with a standard user', () => {
+        LoginPage.signIn(LOGIN_USERS.STANDARD);
+
+        // Wait for the inventory screen and check it
+        expect(SwagOverviewPage.waitForIsShown()).toEqual(
+            true,
+            'Inventory List screen was not shown',
+        );
+    });
+});
